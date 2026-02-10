18233fdf0432fe443ca415f21238c3a2e26c4b9c
peknight <JKpeknight@gmail.com>
2026-02-10 15:40:25 +0800

    update

--- cats-core/shared/src/main/scala/com/peknight/cats/syntax/BooleanSyntax.scala
+++ cats-core/shared/src/main/scala/com/peknight/cats/syntax/BooleanSyntax.scala
@@ -1,16 +1,23 @@
 package com.peknight.cats.syntax
 
-import cats.Monad
 import cats.syntax.applicative.*
 import cats.syntax.flatMap.*
+import cats.syntax.functor.*
+import cats.{Functor, Monad}
 
 trait BooleanSyntax:
   extension [F[_]] (fa: F[Boolean])
     def &&(fb: => F[Boolean])(using Monad[F]): F[Boolean] =
       fa.flatMap(a => if a then fb else false.pure[F])
 
+    def &&(fb: => Boolean)(using Functor[F]): F[Boolean] =
+      fa.map(a => if a then fb else false)
+
     def ||(fb: => F[Boolean])(using Monad[F]): F[Boolean] =
       fa.flatMap(a => if a then true.pure[F] else fb)
+
+    def ||(fb: => Boolean)(using Functor[F]): F[Boolean] =
+      fa.map(a => if a then true else fb)
   end extension
 end BooleanSyntax
 object BooleanSyntax extends BooleanSyntax
